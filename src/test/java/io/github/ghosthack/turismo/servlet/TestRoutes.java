5ab130010c5ceac7a28e6ad4095f9fa416c0559d
ghosthack <837911+ghosthack@users.noreply.github.com>
2026-09-24 05:17:35 -0700

    Move runnable examples to turismo-bootstrap (#49)

--- src/test/java/io/github/ghosthack/turismo/servlet/TestRoutes.java
+++ src/test/java/io/github/ghosthack/turismo/servlet/TestRoutes.java
@@ -1,25 +1,19 @@
-package io.github.ghosthack.turismo.example;
-
+package io.github.ghosthack.turismo.servlet;
 
 import io.github.ghosthack.turismo.action.Action;
 import io.github.ghosthack.turismo.routes.RoutesMap;
 
-public class WebAppRoutes extends RoutesMap {
+/** Routes class loaded by name in {@link ServletTest}. */
+public class TestRoutes extends RoutesMap {
 
     @Override
     protected void map() {
         get("/", new Action() {
+            @Override
             public void run() {
                 print("Hello World!");
             }
         });
-    }
-
-    /**
-     * Minics an application server environment
-     */
-    public static void main(String[] args) throws Exception{
-        JettyHelper.server(8080, JettyHelper.webapp());
     }
 
 }
